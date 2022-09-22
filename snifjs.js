3d5413552a62454f51fc7c569d91205858ada5ec
jjvalega <jjvalega@uninorte.edu.co>
2022-09-21 19:33:26 -0500

    //conexion rcn

--- snifjs.js
+++ snifjs.js
@@ -11,37 +11,32 @@ const data = {
   
 }
 
-//Conexión de de la rds 
+//Conexión rcn
 
-const mysql  = require('mysql2');
-const connection = mysql.createConnection({
-  user: process.env.Rds_user,
-  host: process.env.Rds_Hostname,
-  database: process.env.Rds_DB,
-  password: process.env.Rds_Pass,
-  port: "3306"
+  const mysql  = require('mysql');
+  const connection = mysql.createConnection({
+    user: process.env.Rds_user,
+    host: process.env.Rds_Hostname,
+    database: process.env.Rds_DB,
+    password: process.env.Rds_Pass,
+    port: "3306"
 })  
 connection.connect(function (err){
-  if(err)throw err;
-  console.log("conectado")
+  if (err) {
+    console.error('error conecting: ' + err.stack);
+    return;
+}
+else{
+    console.log("Connected to DB")
+}
 })
 
-let arrayFecha=["","",""];
-let arrayHora=["","",""];
+const insertData = async (Longitud, Latidud, Fecha, Hora) => {
+  
 
-const insertData = async (Longitud, Latitud, Fecha, Hora) => {
-  arrayFecha=Fecha.split("-");
-  var Year=arrayFecha[2];
-  var Month=arrayFecha[1];
-  var Day=arrayFecha[0];
-  arrayHora=Hora.split(":");
-  var Hour=arrayHora[0];
-  var Minute=arrayHora[1];
-  var Second=arrayHora[2];
+  const query = `INSERT INTO disen (Longitud, Latitud, Fecha, Hora) VALUES (${Longitud}, ${Latidud}, "${Fecha}", "${Hora}")`;
 
-  let query = `INSERT INTO disen (Longitud, Latitud, Fecha, Hora) VALUES (${Longitud}, ${Latitud}, ${Day+Month+Year}, ${Hour+Minute+Second})`;
-
-  //console.log(dateComplete)
+  
   connection.query(query, function(err, result){
     if(err)throw err;
     console.log("insertado")
@@ -62,12 +57,12 @@ app.get("/", (req, res) => {
 
 const getRecordInfo = async (Fecha1,Fecha2) => {
 const query = `SELECT * FROM disen WHERE date BETWEEN ${Fecha1} AND ${Fecha2}`;
-const {rows:[{Longitud,Latidud,Fecha, Hora}]} = await connection.query(query);
-  return {Longitud,Latidud,Fecha, Hora}
+const {rows:[{Longitud, Latidud, Fecha, Hora}]} = await connection.query(query);
+  return {Longitud, Latidud, Fecha, Hora}
 
 };
 app.get("/data", async (req, res) => {
-  const query = `SELECT * FROM Datagps ORDER BY ID DESC LIMIT 1`;
+  const query = `SELECT * FROM disen ORDER BY ID DESC LIMIT 1`;
   connection.query(query,(err,result) => {
     if (!err) {
       return res.send(result).status(200);     
@@ -91,12 +86,13 @@ server.on('error', (err) => {
 });
 server.on('message', async (msg, senderInfo) => {
   console.log('Messages received ' + msg)
-  const mensaje = String(msg).split(",")
+  const mensaje = String(msg).split(" ")
   data.Longitud= mensaje[0]
   data.Latitud = mensaje[1]
   data.Fecha = mensaje[2]
-  data.Hora = mensaje[3]
-  insertData(data.Longitud, data.Latitud, data.Fecha, data.Hora)
+  data.Hora = mensaje[4]
+  console.log(mensaje)
+  insertData(data.Longitud,data.Latitud, data.Fecha,data.Hora);
   server.send(msg, senderInfo.port, senderInfo.address, () => {
     console.log(`Message sent to ${senderInfo.address}:${senderInfo.port}`)
   })
@@ -106,5 +102,6 @@ server.on('listening', (req, res) => {
   console.log(`UDP server listening on: ${address.address}:${address.port}`);
 });
 
-server.bind(3000);
+//xdxdxdxdxdxdxd
+server.bind(9001);
 app.listen(9001, () => console.log('Server on port: 9001'));
