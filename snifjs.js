8cc9fc96943d55e78ecab94818e36be0fd64642b
reyessj <112776418+reyessj@users.noreply.github.com>
2022-09-22 05:51:22 -0500

    Polyline

--- snifjs.js
+++ snifjs.js
@@ -37,7 +37,7 @@ else{
 const insertData = async (Longitud, Latitud, Fecha, Hora) => {
   
 
-  const query = `INSERT INTO disen (Longitud, Latitud, Fecha, Hora) VALUES (${Longitud}, ${Latitud}, "${Fecha}", "${Hora}")`;
+  const query = `INSERT INTO Datagps (Longitud, Latitud, Fecha, Hora) VALUES (${Longitud}, ${Latitud}, "${Fecha}", "${Hora}")`;
   console.log("Complete")
   
   connection.query(query, function(err, result){
@@ -60,13 +60,13 @@ app.get("/", (req, res) => {
 
 
 const getRecordInfo = async (Fecha1,Fecha2) => {
-const query = `SELECT * FROM disen WHERE date BETWEEN ${Fecha1} AND ${Fecha2}`;
+const query = `SELECT * FROM Datagps WHERE date BETWEEN ${Fecha1} AND ${Fecha2}`;
 const {rows:[{Longitud, Latitud, Fecha, Hora}]} = await connection.query(query);
   return {Longitud, Latitud, Fecha, Hora}
 
 };
 app.get("/data", async (req, res) => {
-  const query = `SELECT * FROM disen ORDER BY ID DESC LIMIT 1`;
+  const query = `SELECT * FROM Datagps ORDER BY ID DESC LIMIT 1`;
   connection.query(query,(err,result) => {
     if (!err) {
       return res.send(result).status(200);     
