46ab7b53341f3611cc9c60114dafaef5100ee433
jjvalega <jjvalega@uninorte.edu.co>
2022-09-21 19:48:54 -0500

    Update snifjs.js

--- snifjs.js
+++ snifjs.js
@@ -102,6 +102,6 @@ server.on('listening', (req, res) => {
   console.log(`UDP server listening on: ${address.address}:${address.port}`);
 });
 
-//xdxdxdxdxdxdxd
+//xdxd
 server.bind(9001);
 app.listen(9001, () => console.log('Server on port: 9001'));
