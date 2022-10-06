a777480cf66e67eb88a3cbd27e7c00faba03a914
Alvarodf7 <adferreira@uninorte.edu.co>
2022-10-06 07:55:35 -0500

    Update historicorango.js

--- public/historicorango.js
+++ public/historicorango.js
@@ -46,7 +46,7 @@ const showMarkers = async (coords1,coords2) => {
 
 const setRange = () => {
   const range = document.getElementById('range').value;
-  const metersRange = range * 350;
+  const metersRange = range * 300;
   circle.setRadius(metersRange);
 }
 
